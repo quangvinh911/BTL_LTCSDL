7eb67f7398bbd49bfdf731d8be6940b1ea26fd7f
nqvinh <quangvinh00328@gmail.com>
2017-04-18 10:30:00 +0700

    Giao dien

---
+++ BaiTapLonLTCSDL/ChamCong/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChamCong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataGridView datagridView = new DataGridView();
+            datagridView.Dock = DockStyle.Fill;
+            panelDataGridView.Controls.Add(datagridView);
+        }
+
+
+    }
+}
